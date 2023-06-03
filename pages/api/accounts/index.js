6a98de551f916ff64fc21b358f4271af901fa105
Iury Zanonni <izanonni@gmail.com>
2023-06-03 00:17:00 -0300

    refactor: adding switch case in api

--- pages/api/accounts/index.js
+++ pages/api/accounts/index.js
@@ -1,27 +1,29 @@
 const md5 = require("md5");
 
 import User from "../../../src/models/User";
+import { HttpMethod } from "../../../src/utils/HttpMethod";
 
 export default async function handler(req, res) {
-  if (req.method === "GET") {
-    try {
-      const user = User.findOne({ where: { email: req.query.email } });
-      res.status(200).json({ user });
-    } catch (error) {
-      console.log(error);
-    }
-  }
+  switch (req.method) {
+    case HttpMethod.GET:
+      try {
+        const user = User.findOne({ where: { email: req.query.email } });
+        res.status(200).json({ user });
+      } catch (error) {
+        console.log(error);
+      }
+      break;
+    case HttpMethod.POST:
+      const user = req.body;
+      user["password"] = md5(user.password + process.env.NEXT_PUBLIC_MD5_HASH);
 
-  if (req.method === "POST") {
-    const user = req.body;
-    user["password"] = md5(user.password + process.env.NEXT_PUBLIC_MD5_HASH);
+      if (!user.roles) {
+        user["roles"] = "USER";
+      }
 
-    if (!user.roles) {
-      user["roles"] = "USER";
-    }
-
-    await User.create(user)
-      .then((data) => res.status(200).send("User created successfully"))
-      .catch((ex) => res.status(401).send(ex));
+      await User.create(user)
+        .then((data) => res.status(200).send("User created successfully"))
+        .catch((ex) => res.status(401).send(ex));
+      break;
   }
 }
--- pages/api/invoices/index.js
+++ pages/api/invoices/index.js
@@ -1,27 +1,28 @@
 import { authorize } from "../../../src/service/authService";
 import Invoice from "../../../src/models/Invoice";
 import User from "../../../src/models/User";
+import { HttpMethod } from "../../../src/utils/HttpMethod";
 
 export default async function handler(req, res) {
-  if (req.method === "GET") {
-    authorize(req, res, async function () {
-      await Invoice.findAll()
-        .then((data) => res.status(200).send(data))
-        .catch((ex) => res.status(404).send("Invoices not found."));
-    });
-  }
+  switch (req.method) {
+    case HttpMethod.GET:
+      authorize(req, res, async function () {
+        await Invoice.findAll()
+          .then((data) => res.status(200).send(data))
+          .catch((ex) => res.status(404).send("Invoices not found."));
+      });
+      break;
+    case HttpMethod.POST:
+      authorize(req, res, async function () {
+        const invoice = req.body;
 
-  if (req.method === "POST") {
-    authorize(req, res, async function () {
-      const invoice = req.body;
+        const user = await User.findByPk(invoice.userId);
 
-      const user = await User.findByPk(invoice.userId);
+        invoice["user"] = user.dataValues;
 
-      invoice["user"] = user.dataValues;
-
-      await Invoice.create(invoice)
-        .then((data) => res.status(200).send("Invoice created successfully"))
-        .catch((ex) => res.status(401).send(ex));
-    });
+        await Invoice.create(invoice)
+          .then((data) => res.status(200).send("Invoice created successfully"))
+          .catch((ex) => res.status(401).send(ex));
+      });
   }
 }
